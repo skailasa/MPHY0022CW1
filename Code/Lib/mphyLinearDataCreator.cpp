a26b06981a38c9571415b1c115f78264e44e9e68
Sri <srinathkailasa@gmail.com>
2019-02-20 18:02:32 +0000

    Add function to generate fake data
    Added function to generate fake random data using the Linear Data
    Creator object.

--- Code/Lib/mphyLinearDataCreator.cpp
+++ Code/Lib/mphyLinearDataCreator.cpp
@@ -1,4 +1,6 @@
 
+#include <iostream>
+#include <random>
 #include "mphyLinearDataCreator.h"
 
 // constructors/destructors
@@ -12,8 +14,31 @@ _theta_0(t0), _theta_1(t1), _noise(n){};
 
 // methods
 
-mphy::LabelledData mphyLinearDataCreator::GetData() {
-    mphy::LabelledData m;
+mphy::LabelledData merge(std::vector<double> A, std::vector<double> B) {
+    // Helper function to merge together two vectors of doubles
+    mphy::LabelledData target( A.size() < B.size() ? A.size() : B.size());
 
-    return m;
+    for (int i = 0; i < target.size(); i++) {
+        target[i] = std::make_pair(A[i], B[i]);
+    };
+    return target;
+}
+
+mphy::LabelledData mphyLinearDataCreator::GetData() {
+    std::vector<double> xVec;
+    std::vector<double> yVec;
+
+    unsigned int seed = 1;
+    unsigned int nSamples = 1000;
+    std::default_random_engine generator(seed);
+    std::uniform_real_distribution<double> distribution(1,100);
+
+    for (auto i =0; i < nSamples; i++){
+        double x = distribution(generator);;
+        double Y = distribution(generator);;
+        xVec.push_back(x);
+        yVec.push_back(Y);
+    }
+
+    return merge(xVec, yVec);
 }
