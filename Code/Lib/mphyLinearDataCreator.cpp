4bbb19f0594acacf00680b7cb57f9bb6ceb46b06
Sri <srinathkailasa@gmail.com>
2019-02-21 12:50:59 +0000

    Refactor linear data creator, remove helper func
    Refactor the linear data creator, removing a redundant helper
    function to improve readability.

--- Code/Lib/mphyLinearDataCreator.cpp
+++ Code/Lib/mphyLinearDataCreator.cpp
@@ -14,32 +14,29 @@ _theta_0(t0), _theta_1(t1), _noise(n){};
 
 // methods
 
-// Helper function to merge together two vectors of doubles
-mphy::LabelledData merge(std::vector<double> &A, std::vector<double> &B) {
-    mphy::LabelledData target( A.size() < B.size() ? A.size() : B.size());
-
-    for (int i = 0; i < target.size(); i++) {
-        target[i] = std::make_pair(A[i], B[i]);
-    };
-    return target;
-}
-
 // Creates 1000 random points (x, y) which can take values between 1 and 100 inc
 mphy::LabelledData mphyLinearDataCreator::GetData() {
-    std::vector<double> xVec;
-    std::vector<double> yVec;
 
+    // Parameters to seed random data points
     unsigned int seed = 1;
     unsigned int nSamples = 1000;
-    std::default_random_engine generator(seed);
-    std::uniform_real_distribution<double> distribution(1,100);
+    unsigned int low = 1;
+    unsigned int high = 100;
 
-    for (auto i =0; i < nSamples; i++){
-        double x = distribution(generator);;
-        double Y = distribution(generator);;
-        xVec.push_back(x);
-        yVec.push_back(Y);
+    // Random number generator
+    std::default_random_engine generator(seed);
+    std::uniform_real_distribution<double> distribution(low, high);
+
+    // Buffer to store results
+    mphy::LabelledData res(nSamples);
+
+    // Fill buffer with random data
+    for (auto i=0; i < nSamples; i++){
+        double X = distribution(generator);
+        double y = distribution(generator);
+        res[i].first = X;
+        res[i].second = y;
     }
 
-    return merge(xVec, yVec);
+    return res;
 }
