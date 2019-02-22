03c9334d00d636f6d94305d26f6baa7c622e2d46
Sri <srinathkailasa@gmail.com>
2019-02-22 15:06:10 +0000

    Add unit tests for linear data creator
    Add unit tests for linear data creator, completing question 3

--- Testing/mphyLeastSquaresSolverTests.cpp
+++ Testing/mphyLeastSquaresSolverTests.cpp
@@ -6,6 +6,37 @@
 #include "mphyCatchMain.h"
 #include "mphyLinearDataCreator.h"
 
-TEST_CASE( "Test instantiation", "[linear-data-creator]" ) {
-  mphyLinearDataCreator m(0.1, 0.1, 0.1);
+TEST_CASE( "Test linear data creator", "[linear-data-creator]")
+{
+  unsigned int nSamples = 1000;
+  unsigned int seed = 1;
+  int low = 1;
+  int high = 100;
+
+  mphyLinearDataCreator m(seed, nSamples, low, high);
+  mphy::LabelledData data = m.GetData();
+
+  // calculate average of generated data
+  double yavg;
+  double ytotal;
+  double xavg;
+  double xtotal;
+
+  for(int i=0; i<data.size(); i++) {
+    xtotal += data[i].first;
+    ytotal += data[i].second;
+  }
+
+  yavg = ytotal/nSamples;
+  xavg = xtotal/nSamples;
+
+  SECTION("Test correct size") {
+    REQUIRE(data.size() == nSamples);
+  }
+
+  SECTION("Test approximately correct distribution") {
+    REQUIRE(xavg == Approx(50).epsilon(0.05));
+    REQUIRE(yavg == Approx(50).epsilon(0.05));
+  }
 }
+
