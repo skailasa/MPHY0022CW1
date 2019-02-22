6675dbfa752f78cfbe944abb20d95410a37b1ab3
Sri <srinathkailasa@gmail.com>
2019-02-22 16:45:12 +0000

    Add tests for SVD solver
    Add unit tests for SVD solver, same tests as for the normal eq
    solver

--- Testing/mphyLeastSquaresSolverTests.cpp
+++ Testing/mphyLeastSquaresSolverTests.cpp
@@ -6,6 +6,7 @@
 #include "mphyCatchMain.h"
 #include "mphyLinearDataCreator.h"
 #include "mphyNormalEquationSolverStrategy.h"
+#include "mphySVDSolverStrategy.h"
 
 
 TEST_CASE( "Test linear data creator", "[data-creator]")
@@ -68,5 +69,30 @@ TEST_CASE("Test normal equation solver", "[model-solver]")
     REQUIRE(res.second == Approx(0.0033).epsilon(0.05));
   }
 
+}
 
+
+TEST_CASE("Test svd solver", "[model-solver]")
+{
+  // Simulation parameters
+  unsigned int nSamples = 1000;
+  unsigned int seed = 1;
+  int low = 1;
+  int high = 100;
+
+  // Generate random data
+  mphyLinearDataCreator m(seed, nSamples, low, high);
+  mphy::LabelledData data = m.GetData();
+
+  // Calculate result
+  mphy::LinearSolution res = mphySVDSolverStrategy().FitData(data);
+
+  // These expected values are calculated from the simulation parameters
+  SECTION("Test approximately correct y intercept") {
+    REQUIRE(res.first == Approx(50).epsilon(0.05));
+  }
+
+  SECTION("Test approximately correct gradient") {
+    REQUIRE(res.second == Approx(0.0033).epsilon(0.05));
+  }
 }
